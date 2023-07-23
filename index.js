df496bb1dde345258d8e21a7323e321cb5afcbc6
Aakash Nirwan <akashnirwan26@gmail.com>
2023-07-23 19:41:00 +0530

    initial js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+ const pop_left =document.getElementById('pop_song_left')
+ const pop_right =document.getElementById('pop_song_right')
+
+let pop_song =document.getElementsByClassName('pop_song')[0];
+
+ pop_right.addEventListener('click',()=>{
+     pop_song.scrollLeft+=330;
+     
+ })
+ pop_left.addEventListener('click',()=>{
+     pop_song.scrollLeft-=330;
+     
+ })
+
  
